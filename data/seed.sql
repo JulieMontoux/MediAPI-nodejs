a773c83c140492e19c71f1b32a078af16e9773b2
JulieMontoux <julie.montoux@epsi.fr>
2024-05-31 10:06:19 +0200

    feat : update db

--- data/seed.sql
+++ data/seed.sql
@@ -1,5 +1,5 @@
 -- Insérer des auteurs
-INSERT INTO `auteurs` (`nom`, `prenom`, `annee_naissance`, `annee_mort`) VALUES
+INSERT INTO "auteurs" ("nom", "prenom", "annee_naissance", "annee_mort") VALUES
 ('Hugo', 'Victor', 1802, 1885),
 ('Dumas', 'Alexandre', 1802, 1870),
 ('Flaubert', 'Gustave', 1821, 1880),
@@ -12,7 +12,7 @@ INSERT INTO `auteurs` (`nom`, `prenom`, `annee_naissance`, `annee_mort`) VALUES
 ('Maupassant', 'Guy de', 1850, 1893);
 
 -- Insérer des livres
-INSERT INTO `livres` (`titre`, `annee_publication`, `quantite`) VALUES
+INSERT INTO "livres" ("titre", "annee_publication", "quantite") VALUES
 ('Les Misérables', 1862, 5),
 ('Le Comte de Monte-Cristo', 1844, 4),
 ('Madame Bovary', 1857, 3),
@@ -25,14 +25,14 @@ INSERT INTO `livres` (`titre`, `annee_publication`, `quantite`) VALUES
 ('Bel-Ami', 1885, 4);
 
 -- Insérer des relations entre auteurs et livres
-INSERT INTO `auteur_livre` (`id_auteur`, `id_livre`) VALUES
-((SELECT id FROM `auteurs` WHERE nom='Hugo' AND prenom='Victor'), (SELECT id FROM `livres` WHERE titre='Les Misérables')),
-((SELECT id FROM `auteurs` WHERE nom='Dumas' AND prenom='Alexandre'), (SELECT id FROM `livres` WHERE titre='Le Comte de Monte-Cristo')),
-((SELECT id FROM `auteurs` WHERE nom='Flaubert' AND prenom='Gustave'), (SELECT id FROM `livres` WHERE titre='Madame Bovary')),
-((SELECT id FROM `auteurs` WHERE nom='Proust' AND prenom='Marcel'), (SELECT id FROM `livres` WHERE titre='À la recherche du temps perdu')),
-((SELECT id FROM `auteurs` WHERE nom='Zola' AND prenom='Émile'), (SELECT id FROM `livres` WHERE titre='L''Assommoir')),
-((SELECT id FROM `auteurs` WHERE nom='Saint-Exupéry' AND prenom='Antoine de'), (SELECT id FROM `livres` WHERE titre='Le Petit Prince')),
-((SELECT id FROM `auteurs` WHERE nom='Camus' AND prenom='Albert'), (SELECT id FROM `livres` WHERE titre='L''Étranger')),
-((SELECT id FROM `auteurs` WHERE nom='Duras' AND prenom='Marguerite'), (SELECT id FROM `livres` WHERE titre='L''Amant')),
-((SELECT id FROM `auteurs` WHERE nom='Sartre' AND prenom='Jean-Paul'), (SELECT id FROM `livres` WHERE titre='La Nausée')),
-((SELECT id FROM `auteurs` WHERE nom='Maupassant' AND prenom='Guy de'), (SELECT id FROM `livres` WHERE titre='Bel-Ami'));
+INSERT INTO "auteur_livre" ("id_auteur", "id_livre") VALUES
+((SELECT id FROM "auteurs" WHERE nom='Hugo' AND prenom='Victor'), (SELECT id FROM "livres" WHERE titre='Les Misérables')),
+((SELECT id FROM "auteurs" WHERE nom='Dumas' AND prenom='Alexandre'), (SELECT id FROM "livres" WHERE titre='Le Comte de Monte-Cristo')),
+((SELECT id FROM "auteurs" WHERE nom='Flaubert' AND prenom='Gustave'), (SELECT id FROM "livres" WHERE titre='Madame Bovary')),
+((SELECT id FROM "auteurs" WHERE nom='Proust' AND prenom='Marcel'), (SELECT id FROM "livres" WHERE titre='À la recherche du temps perdu')),
+((SELECT id FROM "auteurs" WHERE nom='Zola' AND prenom='Émile'), (SELECT id FROM "livres" WHERE titre='L''Assommoir')),
+((SELECT id FROM "auteurs" WHERE nom='Saint-Exupéry' AND prenom='Antoine de'), (SELECT id FROM "livres" WHERE titre='Le Petit Prince')),
+((SELECT id FROM "auteurs" WHERE nom='Camus' AND prenom='Albert'), (SELECT id FROM "livres" WHERE titre='L''Étranger')),
+((SELECT id FROM "auteurs" WHERE nom='Duras' AND prenom='Marguerite'), (SELECT id FROM "livres" WHERE titre='L''Amant')),
+((SELECT id FROM "auteurs" WHERE nom='Sartre' AND prenom='Jean-Paul'), (SELECT id FROM "livres" WHERE titre='La Nausée')),
+((SELECT id FROM "auteurs" WHERE nom='Maupassant' AND prenom='Guy de'), (SELECT id FROM "livres" WHERE titre='Bel-Ami'));
