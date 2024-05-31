f661349fa5cec7e1d4dfc5fd37800606c227111c
JulieMontoux <julie.montoux@epsi.fr>
2024-05-31 11:37:54 +0200

    feat : update authenticate and books routes

--- App.js
+++ App.js
@@ -4,20 +4,32 @@ const bookRoutes = require('./routes/BooksRoutes');
 
 const app = express();
 const PORT = 8000;
+
 const API_KEY = '8f94826adab8ffebbeadb4f9e161b2dc';
 
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+  });
+
 const authenticate = (req, res, next) => {
-  const apiKey = req.headers['x-api-key'];
-  if (apiKey === API_KEY) {
+  const apiKey = req.headers['authorization'];
+  if (apiKey === 'Bearer ' + API_KEY) {
     next();
   } else {
     res.status(401).json({ error: 'Unauthorized' });
   }
 };
 
-app.use('/api', authenticate, bookRoutes);
+app.use(authenticate);
+app.use('/api', bookRoutes);
+
+app.get('/api', (req, res) => {
+  const data = { message: 'Bienvenue sur MédiAPI' };
+  res.json(data);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/api/`);
